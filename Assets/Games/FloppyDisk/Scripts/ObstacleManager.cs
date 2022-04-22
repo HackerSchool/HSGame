76dfbe1c07f82754f4178e8a91a4263b1af0ee17
PedroB8 <89312339+PedroB8@users.noreply.github.com>
2022-04-22 19:20:43 +0100

    Score System
    Added a score that increases when the player collides with an invisible collider box.

--- Assets/Games/FloppyDisk/Scripts/ObstacleManager.cs
+++ Assets/Games/FloppyDisk/Scripts/ObstacleManager.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class ObstacleManager : MonoBehaviour
 {
     public GameObject obstaclePrefab;
+    public GameObject scorezonePrefab;
     List<GameObject> obstacles = new List<GameObject>();
+
     float xSpawn = 15f;
     int obstacleIndex = 0;
 
@@ -35,26 +37,33 @@ public class ObstacleManager : MonoBehaviour
         Vector3 randomGen = new Vector3(0, Random.Range(-1.4f, 2.1f), 0);
         Vector3 positionBottom = new Vector3(xSpawn, -5, 0) + randomGen;
         Vector3 positionTop = new Vector3(xSpawn, 5, 0) + randomGen;
+        Vector3 scoreZone = new Vector3(xSpawn, 0, 0) + randomGen;
         GameObject currentBottom = Instantiate(obstaclePrefab, positionBottom, transform.rotation);
-        GameObject currentTop = Instantiate(obstaclePrefab, positionTop, transform.rotation);
+        GameObject currentTop = Instantiate(obstaclePrefab, positionTop, transform.rotation);      
+        GameObject currentScoreZone = Instantiate(scorezonePrefab, scoreZone, transform.rotation);         
 
         //adds obstacle to list until the max of 4 pairs and then starts to replace them
+        //also adds a ScoreZone
         if (obstacleIndex < 5)
         {
             obstacles.Add(currentBottom);
             obstacles.Add(currentTop);
+            obstacles.Add(currentScoreZone);         
         }
         else
         {
-            //destroys the oldest pair of obstacles
+            //destroys the oldest pair of obstacles and the oldest ScoreZone
             Destroy(obstacles[0]);
             Destroy(obstacles[1]);
+            Destroy(obstacles[2]);                   
+            obstacles.RemoveAt(0);                
             obstacles.RemoveAt(0);
-            obstacles.RemoveAt(0);
-
-            //adds the newest pair of obstacles
+            obstacles.RemoveAt(0);               
+            
+            //adds the newest pair of obstacles and a ScoreZone
             obstacles.Add(currentBottom);
             obstacles.Add(currentTop);
+            obstacles.Add(currentScoreZone);         
         }
     }
 }
